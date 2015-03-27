3f7213c677bddf8583fa43f11ca08a059d721b65
sandip home <ghadagesandip@gmail.com>
2015-03-27 19:00:45 +0530

    update code

---
+++ src/ComposerWorld/ComposerWorld.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * HelloWorld for demonstration purpose.
+ *
+ * Authors:
+ *  Albert Bakker (hallo[at]abbert[dot]nl)
+ *
+ * Dual licensed under the MIT (MIT-LICENSE.txt)
+ * and GPL (GPL-LICENSE.txt) licenses.
+ */
+namespace ComposerWorld;
+
+/**
+ * Class HelloWorld
+ * @package HelloWorld
+ */
+class ComposerWorld
+{
+	/**
+	 * @return string
+	 */
+	public function sayComposer()
+	{
+		return 'Hello Composer!';
+	}
+}
